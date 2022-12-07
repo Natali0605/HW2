90fbf7c964e21372dc0d8f3b5832128fae639d75
Natali <mishina_nv00@mail.ru>
2022-12-07 20:57:51 +0300

    Сделали дз2

---
+++ Task1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Введите трехзначное число: ");
+int n = int.Parse(Console.ReadLine());
+int numberA = n / 10;
+int result = numberA % 10;
+Console.WriteLine($"Вторая цифра числа " + n + "  - " + result);
